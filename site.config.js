74cbff3ade8bdd5a9f77e5cb8fc81befd04058a6
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:10 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: 'lg정보이용료 이용의 모든 것. 엘지유플러스 사용자 맞춤형 한도 체크와 신속 안전한 거래 시스템을 제공합니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: 'e9c2fb6e7e6ee4011a7e8b5b893e315e845f7efd',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
